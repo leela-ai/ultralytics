05dad0a5958c513b5e2078d36d0799320b67d0a7
hturner08 <hmturner@mit.edu>
2023-06-25 14:12:08 -0400

    Fixed np.bool deprecation bug

--- ultralytics/yolo/utils/plotting.py
+++ ultralytics/yolo/utils/plotting.py
@@ -50,6 +50,9 @@ class Annotator:
     # YOLOv8 Annotator for train/val mosaics and jpgs and detect/hub inference annotations
     def __init__(self, im, line_width=None, font_size=None, font='Arial.ttf', pil=False, example='abc'):
         """Initialize the Annotator class with image and line width along with color palette for keypoints and limbs."""
+        if type(im) == torch.Tensor:
+            im = im.cpu().numpy()
+            im = np.ascontiguousarray(im)
         assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to Annotator() input images.'
         non_ascii = not is_ascii(example)  # non-latin labels, i.e. asian, arabic, cyrillic
         self.pil = pil or non_ascii
